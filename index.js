b4970f7f7fdadbdfe82e40e35964fa6e40ada8b1
Seyed Ketabchi <seyed.ketabchi@gmail.com>
2017-03-02 10:01:45 -0800

    commit new changes for the cache version

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
 
-const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
 
-const tableName = process.env.TABLE_NAME;
+var Ecad = require('ecad');
+var AWS = require('aws-sdk');
+var Memcached = require('memcached');
+
+const cacheName = process.env.CACHE_ENDPOINT;
 
 const createResponse = (statusCode, body) => {
     
@@ -14,10 +16,28 @@ const createResponse = (statusCode, body) => {
     }
 };
 
-exports.get = (event, context, callback) => {
+exports.get = (event,context,callback) => {
+    
+    var endpoints = [cacheName];
+    var client = new Ecad({endpoints: endpoints, timeout: 10000});
+    client.fetch(function (err, hosts) {
+        console.log(err);
+        console.log(hosts);
+        var connectTo = hosts[parseInt(Math.random() * hosts.length)];
+
+        var memcached = new Memcached(connectTo);
+        memcached.set(event.key1, event.value1, 100, function (err) {
+            memcached.get(event.key1, function (err, data) {
+                console.log(data);
+            });
+            callback(null);
+        });
+    });
+};
+exports.nothing = (event, context, callback) => {
     
     let params = {
-        TableName: tableName,
+        TableName: cacheName,
         Key: {
             id: event.pathParameters.resourceId
         }
@@ -34,54 +54,6 @@ exports.get = (event, context, callback) => {
         callback(null, createResponse(200, data.Item.doc));
     }).catch( (err) => { 
         console.log(`GET ITEM FAILED FOR doc = ${data.Item.doc}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err));
-    });
-};
-
-exports.put = (event, context, callback) => {
-    
-    let item = {
-        id: event.pathParameters.resourceId,
-        doc: event.body
-    };
-
-    let params = {
-        TableName: tableName,
-        Item: item
-    };
-    
-    let dbPut = (params) => { return dynamo.put(params).promise() };
-    
-    dbPut(params).then( (data) => {
-        console.log(`PUT ITEM SUCCEEDED WITH doc = ${item.doc}`);
-        callback(null, createResponse(200, null));
-    }).catch( (err) => { 
-        console.log(`PUT ITEM FAILED FOR doc = ${item.doc}, WITH ERROR: ${err}`);
-        callback(null, createResponse(500, err)); 
-    });
-};
-
-exports.delete = (event, context, callback) => {
-    
-    let params = {
-        TableName: tableName,
-        Key: {
-            id: event.pathParameters.resourceId
-        },
-        ReturnValues: 'ALL_OLD'
-    };
-    
-    let dbDelete = (params) => { return dynamo.delete(params).promise() };
-    
-    dbDelete(params).then( (data) => {
-        if (!data.Attributes) {
-            callback(null, createResponse(404, "ITEM NOT FOUND FOR DELETION"));
-            return;
-        }
-        console.log(`DELETED ITEM SUCCESSFULLY WITH id = ${event.pathParameters.resourceId}`);
-        callback(null, createResponse(200, null));
-    }).catch( (err) => { 
-        console.log(`DELETE ITEM FAILED FOR id = ${event.pathParameters.resourceId}, WITH ERROR: ${err}`);
         callback(null, createResponse(500, err));
     });
 };
